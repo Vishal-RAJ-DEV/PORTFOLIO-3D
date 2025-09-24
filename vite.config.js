4da3c47e073d538938e5209bd02fe1ebf1b2aee5
Vishal-RAJ-DEV <user.vishalraj140706@gmail.com>
2025-09-24 15:44:20 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          three: ['three'],
-          drei: ['@react-three/drei'],
-          fiber: ['@react-three/fiber'],
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1600
-  },
-  optimizeDeps: {
-    include: ['three']
-  }
 })
